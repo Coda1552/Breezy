cb5d4be7001d215ba89c9dc71ca53bd813759911
Cody <cody.koelliker@outlook.com>
2022-09-12 22:46:30 -0600

    remove todo

--- src/main/java/coda/whooosh/common/WindDirectionSavedData.java
+++ src/main/java/coda/whooosh/common/WindDirectionSavedData.java
@@ -10,7 +10,7 @@ import net.minecraft.world.level.saveddata.SavedData;
 
 import java.util.Random;
 
-// todo, add wind compass, make wind change from day to day, add sand bags
+// todo, add wind compass, add sand bags
 public class WindDirectionSavedData extends SavedData {
     private static final int INTERVAL = 12;
     private static final Direction[] directions = new Direction[INTERVAL];
