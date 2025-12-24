b952bd836c50bce8e41aaecd161f8ea8e5964066
DavigJ <david.j.jung@gmail.com>
2025-12-24 06:30:36 +0400

    fixes #40

--- src/main/java/codyhuh/breezy/core/registry/BreezyItems.java
+++ src/main/java/codyhuh/breezy/core/registry/BreezyItems.java
@@ -11,5 +11,5 @@ public class BreezyItems {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Breezy.MOD_ID);
 
     public static final RegistryObject<Item> HOT_AIR_BALLOON = ITEMS.register("hot_air_balloon", () -> new HotAirBalloonItem(new Item.Properties().stacksTo(1)));
-    public static final RegistryObject<Item> GUST_GAUGE = ITEMS.register("gust_gauge", () -> new Item(new Item.Properties().stacksTo(1)));
+    public static final RegistryObject<Item> GUST_GAUGE = ITEMS.register("gust_gauge", () -> new Item(new Item.Properties()));
 }
